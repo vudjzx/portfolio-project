198ed8ebcd1b1aac3e9eef6d1abcc262f9418458
vudjzx <vudjzx@gmail.com>
2021-05-25 17:34:03 -0500

    Added the navbar.

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Nav, Navbar } from 'react-bootstrap'
 const NavigationBar = () => {
     return (
         <div>
-            This is the nav bar
+            <Navbar 
+            bg="light"
+            variant="light"
+            sticky="top"
+            expand="sm"
+            className="justify-content-between"
+            >
+                <Navbar.Brand href="#home">Víctor Durán</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"  />
+                <Navbar.Collapse id="basic-navbar-nav" >
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     )
 }
